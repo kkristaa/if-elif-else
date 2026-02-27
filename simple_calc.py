58f8732d0780bef0915e148ce76badee57e88d00
“kkristaa” <khknyschuk30@gmail.com>
2026-02-28 00:16:37 +0200

    first commit

---
+++ simple_calc.py
@@ -0,0 +1,8 @@
+#Простий калькулятор
+first_number = float(input("Введіть перше число:")) #Просимо користувача ввести число1 та переводимо у float
+second_number = float(input("Введіть друге число:")) #Просимо користувача ввести число2 та переводимо у float
+
+result = first_number + second_number #Знаходимо суму
+
+print(first_number, "+", second_number, "=", result) #Виводимо результат додавання чисел користувача
+
